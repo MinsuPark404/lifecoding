9fe29f750f72f1d32b8b366fb64400668a619acc
PMS <parkms6490@gmail.com>
2023-08-28 09:12:08 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,59 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
+
+// 컴포넌트 -> 사용자 정의 태그
+function Header(props){
+  console.log("props:", props, "제목:", props.title);
+  return (
+    <header>
+      <h1><a href="/" onClick={(e)=>{
+        e.preventDefault();
+        props.onChange();
+      }}>{props.title}</a></h1>
+    </header>
+  )
+}
+
+function Nav(props){
+  const list = []
+  console.log("중간로그:",props.topics)
+  props.topics.map(item => {
+    list.push(<li key={item.id}><a href={item.id}>{item.title}</a></li>)
+  })
+  return (
+    <nav>
+      <ol>
+        {list}
+      </ol>
+    </nav>
+  )
+}
+
+const Article = (props) => {
+  return (
+    <article>
+      <h2>{props.title}</h2>
+      {props.body}
+    </article>
+  )
+}
+
+
 
 function App() {
+  const topics = [
+    { id:1, title:'html', body:'html is ...' },
+    { id:2, title:'css', body:'css is ...' },
+    { id:3, title:'javascrip', body:'javascrip is ...' },
+  ];
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header title="WEB" onChange={()=>{
+        alert('Header')
+      }}></Header>
+      <Nav topics={topics}></Nav>
+      <Article title="Welcome" body="Hello, WEB"/>
     </div>
   );
 }
